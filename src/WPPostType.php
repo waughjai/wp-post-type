cc3f21a7665cfdbf1bac13f2ca51a79f3fbdcfe1
J. J. W. Mezun <jjwmezun@gmail.com>
2021-04-02 18:10:16 -0700

    Fully implement labels and add way to override labels

--- src/WPPostType.php
+++ src/WPPostType.php
@@ -65,11 +65,7 @@ class WPPostType
 				(
 					$this->slug,
 					[
-						'labels' =>
-						[
-							'name' => __( $this->name ),
-							'singular_name' => __( $this->other_arguments->get( 'singular_name' ) )
-						],
+						'labels' => $this->generateLabels(),
 						'public' => $this->other_arguments->get( 'public' ),
 						'has_archive' => $this->other_arguments->get( 'has_archive' ),
 						'supports' => $this->getSupports(),
@@ -220,6 +216,43 @@ class WPPostType
 			return $columns;
 		}
 
+		private function generateLabels() : array
+		{
+			// Merge default labels with user-specified labels.
+			// User-specified labels latter so they override defaults.
+			$singularName = $this->other_arguments->get( 'singular_name' );
+			return array_merge
+			(
+				[
+					'name' => __( $this->name ),
+					'singular_name' => __( $this->other_arguments->get( 'singular_name' ) ),
+					'menu_name'             => _x( $this->name, 'Admin Menu text', 'textdomain' ),
+					'name_admin_bar'        => _x( $singularName, 'Add New on Toolbar', 'textdomain' ),
+					'add_new'               => __( 'Add New', 'textdomain' ),
+					'add_new_item'          => __( 'Add New ' . $singularName, 'textdomain' ),
+					'new_item'              => __( 'New ' . $singularName, 'textdomain' ),
+					'edit_item'             => __( 'Edit ' . $singularName, 'textdomain' ),
+					'view_item'             => __( 'View ' . $singularName, 'textdomain' ),
+					'all_items'             => __( 'All ' . $this->name, 'textdomain' ),
+					'search_items'          => __( 'Search ' . $this->name, 'textdomain' ),
+					'parent_item_colon'     => __( 'Parent ' . $this->name . ':', 'textdomain' ),
+					'not_found'             => __( 'No ' . strtolower( $this->name ) . ' found.', 'textdomain' ),
+					'not_found_in_trash'    => __( 'No ' . strtolower( $this->name ) . ' found in Trash.', 'textdomain' ),
+					'featured_image'        => _x( $singularName . ' Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+					'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+					'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+					'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+					'archives'              => _x( $singularName . ' archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+					'insert_into_item'      => _x( 'Insert into ' . strtolower( $singularName ), 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+					'uploaded_to_this_item' => _x( 'Uploaded to this ' . strtolower( $singularName ), 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+					'filter_items_list'     => _x( 'Filter ' . strtolower( $this->name ) . ' list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+					'items_list_navigation' => _x( $this->name . ' list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+					'items_list'            => _x( $this->name . ' list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' )
+				],
+				$this->other_arguments->get( 'labels' )
+			);
+		}
+
 		private static function argumentDefaults( string $slug, string $name ) : array
 		{
 			return
@@ -234,7 +267,8 @@ class WPPostType
 				'custom_toc' => [ 'value' => [] ],
 				'unset_toc' => [ 'value' => [] ],
 				'taxonomies' => [ 'value' => [] ],
-				'capabilities' => [ 'value' => [] ]
+				'capabilities' => [ 'value' => [] ],
+				'labels' => [ 'value' => [] ]
 			];
 		}
 
